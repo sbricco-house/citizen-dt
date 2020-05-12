5df8b309b08f331523121f2109a365ef4215668d
Andrea <petretiandrea@gmail.com>
2020-05-12 12:19:03 +0200

    some refactor + authentication + authorization mock. Removed CitizenController, many problem with executor

--- model/src/main/scala/it/unibo/core/microservice/vertx/BaseVerticle.scala
+++ model/src/main/scala/it/unibo/core/microservice/vertx/BaseVerticle.scala
@@ -5,10 +5,9 @@ import io.vertx.scala.ext.web.Router
 
 import scala.concurrent.Future
 
-abstract class BaseVerticle(private val controller: RouterController, port : Int = 8080, host : String = "0.0.0.0") extends ScalaVerticle {
+abstract class BaseVerticle(port : Int = 8080, host : String = "0.0.0.0") extends ScalaVerticle {
   override def startFuture(): Future[Unit] = {
     val router = createRouter()
-    controller.init(vertx)
     vertx
       .createHttpServer()
       .requestHandler(router.accept)
