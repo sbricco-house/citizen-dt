cd0bf32b114de9c13c705541c354ff6717278cfd
Gianluca <gianluca.aguzzi.studio@gmail.com>
2020-06-05 18:52:02 +0200

    remove role based

--- permission/src/main/scala/it/unibo/service/permission/MockAuthorization.scala
+++ permission/src/main/scala/it/unibo/service/permission/MockAuthorization.scala
@@ -1,6 +1,6 @@
 package it.unibo.service.permission
 
-import it.unibo.core.authentication.{SystemUser, TokenIdentifier}
+import it.unibo.core.authentication.TokenIdentifier
 import it.unibo.core.data.{DataCategory, DataCategoryOps, GroupCategory, LeafCategory}
 import it.unibo.core.microservice.{Fail, FutureService, Response}
 import it.unibo.core.parser.DataParserRegistry
@@ -16,26 +16,27 @@ object MockAuthorization {
     override def authorizedWriteCategories(who: TokenIdentifier, citizen: String): FutureService[Seq[DataCategory]] = FutureService.response(registry.supportedCategories)
   }
   /*
+  TODO pensa come fare il role based
   def roleBased(citizenWritePermission : Set[DataCategory], otherWritePermission : Map[String, Set[DataCategory]], otherReadPermission : Map[String, Set[DataCategory]], allCategories : DataCategory *) = new AuthorizationService {
-    override def authorizeRead(who: TokenIdentifier, citizen: String, category: DataCategory): FutureService[DataCategory] = who.identifier match {
+    override def authorizeRead(who: TokenIdentifier, citizen: String, category: DataCategory): FutureService[DataCategory] = who match {
       case `citizen` => FutureService.response(category)
       case other => checkRWOther(otherReadPermission, other, category)
     }
-    override def authorizeWrite(who: TokenIdentifier, citizen: String, category: DataCategory): FutureService[DataCategory] = who.identifier match {
+    override def authorizeWrite(who: TokenIdentifier, citizen: String, category: DataCategory): FutureService[DataCategory] = who match {
       case `citizen` if citizenWritePermission.contains(category) => FutureService.response(category)
       case `citizen` => FutureService.fail(Unauthorized(s"User $citizen cannot access to $citizen"))
       case other => checkRWOther(otherWritePermission, other, category)
     }
-    override def authorizedReadCategories(who: TokenIdentifier, citizen: String): FutureService[Seq[DataCategory]] = who.role match {
+    override def authorizedReadCategories(who: TokenIdentifier, citizen: String): FutureService[Seq[DataCategory]] = who match {
       case `citizen` => FutureService.response(allCategories)
       case other =>getCategoriesFromOther(otherReadPermission, other)
     }
-    override def authorizedWriteCategories(who: TokenIdentifier, citizen: String): FutureService[Seq[DataCategory]] = who.role match {
+    override def authorizedWriteCategories(who: TokenIdentifier, citizen: String): FutureService[Seq[DataCategory]] = who match {
       case `citizen` => FutureService.response(citizenWritePermission.toSeq)
       case other => getCategoriesFromOther(otherWritePermission, other)
     }
 
-    private def checkRWOther(mapPermission : Map[String, Set[DataCategory]], who : String, category : DataCategory) : FutureService[DataCategory] = {
+    private def checkRWOther(mapPermission : Map[String, Set[DataCategory]], who : TokenIdentifier, category : DataCategory) : FutureService[DataCategory] = {
       mapPermission.get(who).filter(isOperationEnabled(_, category)) match {
         case None => FutureService.fail(Unauthorized(s"User $who cannot access to"))
         case Some(_) => FutureService.response(category)
@@ -47,7 +48,7 @@ object MockAuthorization {
       case LeafCategory(name, _) => DataCategoryOps.allChild(requestCategory).exists(leaf => leaf.name == name)
     }
 
-    private def getCategoriesFromOther(mapPermission : Map[String, Set[DataCategory]], who : String) : FutureService[Seq[DataCategory]] = {
+    private def getCategoriesFromOther(mapPermission : Map[String, Set[DataCategory]], who : TokenIdentifier) : FutureService[Seq[DataCategory]] = {
       mapPermission
         .get(who)
         .map(_.toSeq)
@@ -55,7 +56,6 @@ object MockAuthorization {
         .getOrElse(FutureService.fail(Unauthorized(s"User $who cannot access")))
     }
   }
-  TODO
   */
 }
 
